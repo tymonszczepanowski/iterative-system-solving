bf54ada0e59ea0377f293a7c8c94fbb1a062d978
Tymon Szczepanowski <tymon.szczepanowski@gmail.com>
2021-07-09 19:00:23 +0200

    Changed testing values

--- main.cpp
+++ main.cpp
@@ -2,15 +2,18 @@
 #include "Equation.h"
 
 void test1(){
-    std::vector<double> row1 = {1, 2, -1};
-    std::vector<double> row2 = {1, -1, 2};
-    std::vector<double> row3 = {2, 2, 2};
+    std::cout << "Test 1, exact solution: (1, 2, -1, 1)" << std::endl;
+    std::vector<double> row1 = {10, -1, 2, 0};
+    std::vector<double> row2 = {-1, 11, -1, 3};
+    std::vector<double> row3 = {2, -1, 10, -1};
+    std::vector<double> row4 = {0, 3, -1, 8};
     std::vector<std::vector<double>> matrixA;
     matrixA.push_back(row1); 
     matrixA.push_back(row2); 
     matrixA.push_back(row3); 
+    matrixA.push_back(row4); 
 
-    std::vector<double> vectorb = {2, 5, 12};
+    std::vector<double> vectorb = {6, 25, -11, 15};
     
     Equation* Eq = new Equation(matrixA, vectorb);
     Eq->solve();
